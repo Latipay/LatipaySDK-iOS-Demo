38bda9ead62ba5526f5d3829492bc42fb65f6fb8
TonnyLTP <32822885+TonnyLTP@users.noreply.github.com>
2017-11-13 21:12:01 +1300

    Initial Commit

---
+++ Latipay_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Latipay_Demo
+//
+//  Created by Tonny on 13/11/17.
+//  Copyright © 2017 Latipay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
